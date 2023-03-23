c2735492ded68f5b905a68d148b4b97602a60dc0
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-23 11:08:35 -0600

    agregar lista de categorias y funcionalidad de agregar nueva

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,4 +1,12 @@
+import {useState} from 'react'
+
 export const GifApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+    const onAddCategory = () => {
+        setCategories([...categories, 'Valorant']);
+    }
+
     return (
         <>
             {/* Titulo */}
@@ -8,6 +16,14 @@ export const GifApp = () => {
 
             {/* Listado de Gifs */}
                 {/* Gif Item */}
+            <button onClick={onAddCategory}>Agregar</button>
+            <ol>
+                { 
+                    categories.map(category => {
+                        return <li key={ category }>{category}</li>
+                    })
+                }
+            </ol>
         </>
     );
 };
