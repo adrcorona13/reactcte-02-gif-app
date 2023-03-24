c006cd2e1c77eaf3a7f810963d3a48587e63963e
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-24 17:42:08 -0600

    agregar useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,21 @@
+import { useEffect, useState } from "react";
 import { getgifs } from "../helpers/getGifs";
 
 export const GifGrid = ({category}) => {
 
-    getgifs(category);
+    const [counter, setCounter] = useState(0);
 
+    getgifs(category);
+    
+    useEffect( () => {
+        getgifs(category);
+    }, [])
+    
     return (
         <>
             <h3>{category}</h3>
-            
+            <h5>{counter}</h5>
+            <button onClick={() => setCounter(counter+1)}>+1</button>
         </>
     )
 }
